3fac040ee5fa2b33121740467be2f1065843cba4
BenjGundersen <benjgunder@gmail.com>
2023-01-05 16:20:06 -0700

    Followed the assignment instructions.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage: ");
+        string stringGradePercentage = Console.ReadLine();
+        float gradePercentage = float.Parse(stringGradePercentage);
+        string letter;
+        if (gradePercentage >= 90)
+        {
+            //Console.WriteLine("You have A!");
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            //Console.WriteLine("You have B!");
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            //Console.WriteLine("You have C!");
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            //Console.WriteLine("You have D!");
+            letter = "D";
+        }
+        else
+        {
+            //Console.WriteLine("KYS!!!");
+            letter = "F";
+        }
+        Console.WriteLine($"You have a {letter}");
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You pass the class!");
+        }
+        else
+        {
+            Console.WriteLine("You do not pass!");
+        }
     }
 }
