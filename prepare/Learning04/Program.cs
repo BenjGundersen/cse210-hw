edfe28639e4be9e91d0da10818737bfa625d2575
BenjGundersen <benjgunder@gmail.com>
2023-02-14 16:06:20 -0700

    did the learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        Assignment theAssignment = new Assignment("Ben", "math");
+        Console.WriteLine(theAssignment.getSummary());
+        MathAssignment theMathAssignment = new MathAssignment("Ben", "math","Section 7","69");
+        Console.WriteLine(theMathAssignment.getHomeworkList());
+        WritingAssignment theWritingAssignment = new WritingAssignment("Ben", "math", "how to not hate youself while doing calc!");
+        Console.WriteLine(theWritingAssignment.getWritingInformation());
     }
 }
