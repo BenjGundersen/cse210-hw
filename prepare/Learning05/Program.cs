7f029149a711bb178b510e79e8336de7b09f9fa2
BenjGundersen <benjgunder@gmail.com>
2023-02-28 15:51:11 -0700

    did the prepare learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square theSquare = new Square ("blue", 5);
+        Rectangle theRectangle = new Rectangle ("red" , 10, 5);
+        Circle theCircle = new Circle ("orange", 6.9);
+
+        List<Shape> theShapes = new List<Shape>();
+        theShapes.Add(theSquare);
+        theShapes.Add(theRectangle);
+        theShapes.Add(theCircle);
+
+        foreach (var shape in theShapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
